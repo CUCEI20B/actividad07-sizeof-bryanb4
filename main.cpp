7a4346120ceccf937755fe1c8d57ad93c76a6b2a
bryan oregel <bryan.martinez1191@alumnos.udg.mx>
2020-10-05 22:57:32 -0500

    cambie el codigo hola mundo apara que me diera los bits con sizeof

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+int main(){
+    string cadena;
 
-    return 0;
+    getline(cin, cadena);
+    cout << cadena << endl;
+    if (cadena == "char")
+    {
+        cout << sizeof(char) << endl;
+    }
+
+    else if(cadena == "int")
+    {
+        cout << sizeof(int) << endl;
+    }
+    else if (cadena == "short")
+    {
+        cout << sizeof(short) << endl;
+    }
+    else if(cadena == "float")
+    {
+        cout << sizeof(float) << endl;
+    }
+    else if(cadena == "double")
+    {
+        cout << sizeof(double) << endl;
+    }
+    else if(cadena == "long")
+    {
+        cout << sizeof(long) << endl;
+    }
 }
